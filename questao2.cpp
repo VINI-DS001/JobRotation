a85ab40a6d859f29aaa63c64d1c80d2a5fc46f53
Vinicius Souza <vinidisz@gmail.com>
2023-03-03 11:55:57 -0300

    Envio dos arquivos

---
+++ questao2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t1 = 0, t2 = 1, proxNum = 0, n;
+
+    cout << "Insira um numero: ";
+    cin >> n;
+
+    // displays the first two terms which is always 0 and 1
+    cout << "Sequencia de Fibonacci: " << t1 << ", " << t2 << ", ";
+
+    proxNum = t1 + t2;
+
+    while(proxNum <= n) {
+        cout << proxNum << ", ";
+        t1 = t2;
+        t2 = proxNum;
+        proxNum = t1 + t2;
+    }
+    
+    if (n==t1 || n==t2){
+        cout << "\nEh um Numero de fibonacci";
+    }else{
+        cout << "\nNao eh um numero de Fibonacci";
+    }
+            
+    return 0;
+}
